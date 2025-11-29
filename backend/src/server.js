db19900928ccc48fd86749c5f71f8aa41091d013
AshishRajx7 <ashishrajcr7@gmail.com>
2025-11-29 15:30:45 +0530

    for separate deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,22 @@
 import express from 'express';
 import path from 'path';
-
-import {ENV} from './lib/env.js';
+import { ENV } from './lib/env.js';
 
 const app = express();
 
-const __dirname = path.resolve();
-
 app.get('/health', (req, res) => {
-    res.status(200).json({msg:"api is running"});
+  res.status(200).json({ msg: "api is running" });
 });
+
 app.get('/books', (req, res) => {
-    res.status(200).json({msg:"this is books endpoint"});
+  res.status(200).json({ msg: "this is books endpoint" });
 });
 
-//make our app ready for deployment
-if (ENV.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+// IMPORTANT
+// Remove serving frontend because Render backend DOES NOT contain frontend/dist
+// Your frontend will be deployed separately on Vercel
+// So no static file serving here
 
-    app.get('/{*any}', (req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-    });
-}
-
-app.listen(ENV.PORT, async () => console.log(`Server started on port ${ENV.PORT} in ${ENV.NODE_ENV} mode`));
+app.listen(ENV.PORT, () => {
+  console.log(`Server started on port ${ENV.PORT} in ${ENV.NODE_ENV} mode`);
+});
